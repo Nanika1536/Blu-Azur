39cb5cfb8303f2b80b723c7cd6db24a64cdad07f
DINASOA NELLY <dinasoa@MacBook-Pro-de-DINASOA.local>
2025-09-25 18:15:51 +0300

    commit 7

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import Hero from './Hero.jsx'
 import Features from './Features.jsx'
 import Section from './Section.jsx'
+import Maps from './Maps.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Hero />
     <Features />
     <Section />
+    <Maps />
   </StrictMode>,
 )
